211d70a429881d0efa63f2b584ec01a7d22efaab
MGM103 <marcus.marinelli3@gmail.com>
2025-05-05 22:58:15 +1000

    Fixing version error for neotree

--- init.lua
+++ init.lua
@@ -199,7 +199,7 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
+			{ "MunifTanjim/nui.nvim", commit = "8d3bce9764e627b62b07424e0df77f680d47ffdb" },
 			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		lazy = false, -- neo-tree will lazily load itself
